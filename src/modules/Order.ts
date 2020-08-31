62012d6208f84dcfd1c24e3d27c73018cbb12433
Ignacio Mazzara <nachomazzara@gmail.com>
2020-08-31 00:43:19 -0300

    feat: address

--- src/modules/Order.ts
+++ src/modules/Order.ts
@@ -6,9 +6,9 @@ export const CANCELLED = 'cancelled'
 
 export function getAddressByNetwork(network: string): Address {
   if (network == 'mainnet') {
-    throw 'No Address specified at getAddressByNetwork for mainnet'
+    return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
   } if (network == 'rinkeby') {
-    return Address.fromString('0xb6548416b3db631e5351fa8ab227f85608050fdb')
+    return Address.fromString('0xd412054cca18a61278ced6f674a526a6940ebd84')
   }
 
   throw 'No Address specified'
